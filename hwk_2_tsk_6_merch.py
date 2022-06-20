270e01557bc5effb2b188a54138ef232a63d367b
Oleg Bryzgalov <oleg.privatepost@yandex.ru>
2022-06-20 22:37:06 +0300

    2nd Homework task 6 complete

--- hwk_2_tsk_6_merch.py
+++ hwk_2_tsk_6_merch.py
@@ -5,14 +5,26 @@
 shop_price = []
 cat_numr = len(shop_price) + 1
 
+
 n = int(input("Сколько номеров заводим, шеф?: "))
 for _ in range(n):
     name = input("Имя товара: ")
     price = input("Цена товара: ")
     qnty = input("Количество его: ")
     ummt = input("Единицы измерения: ")
-    shop_price.append((cat_numr, {'Наименование' : name, 'Цена: ' : price, 'Кол-во' : qnty, 'Ед-изм': ummt}))
+    ftrs = {'Наименование' : name, 'Цена' : price, 'Кол-во' : qnty, 'Ед-изм': ummt}
+    shop_price.append((cat_numr, ftrs))
     cat_numr += 1
 
 print('\n'.join(map(str, shop_price)))
+
+shop_analytics = {'Наименование' : [], 'Цена' : [], 'Кол-во' : [], 'Ед-изм': []}
+for s in shop_price:
+    shop_analytics['Наименование'].append(s[1]['Наименование'])
+    shop_analytics['Цена'].append(s[1]['Цена'])
+    shop_analytics['Кол-во'].append(s[1]['Кол-во'])
+    if s[1]['Ед-изм'] not in shop_analytics['Ед-изм']:
+        shop_analytics['Ед-изм'].append(s[1]['Ед-изм'])
+
+print(shop_analytics)
 
