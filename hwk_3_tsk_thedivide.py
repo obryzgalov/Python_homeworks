974c1d9d58c5f68f12c32f6de3de3ba392d39713
Oleg Bryzgalov <oleg.privatepost@yandex.ru>
2022-06-22 15:05:10 +0300

    3rd Homework task 2 complete

---
+++ hwk_3_tsk_thedivide.py
@@ -0,0 +1,23 @@
+# Реализовать функцию деления с учетом ошибки деления на 0.
+
+"""
+Возвращает частное от деления
+x -  делимое,
+y - делитель по умолчанию
+
+"""
+
+def my_div(x,y):
+    try:
+        return int(x)/int(y)
+    except ZeroDivisionError: #исключение с делением на 0
+        return "Zero division Error!"
+    except ValueError: #исключение с типом данных
+        return "Wrong Data! Enter the numbers!"
+
+x = input("Enter x ")
+y = input("Enter y ")
+
+print(my_div(x, y))
+
+
