fb15e83f6a40a69a42addf3c5c83d95921431fd8
Oleg Bryzgalov <oleg.privatepost@yandex.ru>
2022-06-22 17:01:33 +0300

    3rd Homework task 3 complete

--- hwk_3_tsk_3_maxsumm.py
+++ hwk_3_tsk_3_maxsumm.py
@@ -3,11 +3,13 @@
 Функция принимает 3 позиц. аргумента и суммирует 2 наибольших
 
 """
-def max_summ(*args):
-    return sum(args) - min(args)
 
-x = int(input('enter x '))
-y = int(input('enter y '))
-z = int(input('enter z '))
+
+def max_summ(*args):
+        return sum(args) - min(args)
+
+x = float(input('enter x '))
+y = float(input('enter y '))
+z = float(input('enter z '))
 
 print(max_summ(x, y, z))
