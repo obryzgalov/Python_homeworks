053cf53443bdaa5f1c56aed45da90d249c1f1522
Oleg Bryzgalov <oleg.privatepost@yandex.ru>
2022-06-24 15:59:46 +0300

    3rdHomework task 6-7

---
+++ hwk_3_tsk_6_7.py
@@ -0,0 +1,11 @@
+def int_func(wrd):
+    return wrd.capitalize()
+
+
+ent_list = input('enter string w/split: ').split()
+for i, wrd in enumerate(ent_list):
+    ent_list[i] = int_func(wrd)
+
+print(' '.join(ent_list))
+
+
