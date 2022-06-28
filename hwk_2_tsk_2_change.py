596d00d67f32731b1590e728397b7a5e3861f449
Oleg Bryzgalov <oleg.privatepost@yandex.ru>
2022-06-28 14:51:32 +0300

    2nd rules correction

--- hwk_2_tsk_2_change.py
+++ hwk_2_tsk_2_change.py
@@ -2,8 +2,10 @@
 
 new_list = list(input("Enter elements: ").split())
 print("Original list is: ", (new_list))
+
 for i in range(1, len(new_list), 2):
     x = new_list[i - 1]
     new_list[i - 1] = new_list[i]
     new_list[i] = x
+
 print("New list is: ", (new_list))
