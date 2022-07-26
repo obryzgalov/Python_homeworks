120a217d7a65571a68e5d98d0cd66721fdb62dcf
Oleg Bryzgalov <oleg.privatepost@yandex.ru>
2022-07-26 21:28:27 +0300

    8

--- Homework_8/Hwk_8_tsk_4_5_6_StorageProject.py
+++ Homework_8/Hwk_8_tsk_4_5_6_StorageProject.py
@@ -3,6 +3,8 @@ class Storage:
 
 
 class OfficeEquip(Storage):
+    name = "baseclass"  # переменная для всех классов
+
     def __init__(self, d_model, d_year, amount):
         self.d_model = d_model
         self.d_year = d_year
@@ -11,58 +13,42 @@ class OfficeEquip(Storage):
         self.s_office = {'office_1': 0, 'office_2': 0}
         self.n_office = {'office_1': 0, 'office_2': 0}
 
-
-class Printers(OfficeEquip):
-    def __init__(self, cartridge, wireless, d_model, d_year, amount):
-        super().__init__(d_model, d_year, amount)
-        self.cartridge = cartridge
-        self.wireless = wireless
-
     def __str__(self):
-        return f"на складе {self.amount} принтеров"
+        return f"на складе {self.amount} {self.name}"
 
     def sended(self, send, office):
         if str(send).isdigit() and self.amount >= send:
             self.amount -= send
             self.p_office[office] += send
-            return f'на складе {self.amount} принтеров в офисе {office} {self.p_office[office]} принтеров'
+            return f'на складе {self.amount} принтеров в офисе {office} {self.p_office[office]} {self.name}'
         else:
             return f'we cant'
 
 
+class Printers(OfficeEquip):
+    name = "принтеров"
+
+    def __init__(self, cartridge, wireless, d_model, d_year, amount):
+        super().__init__(d_model, d_year, amount)
+        self.cartridge = cartridge
+        self.wireless = wireless
+
+
 class Netbooks(OfficeEquip):
+    name = "нетбуков"
+
     def __init__(self, bat_mah, screen_inches, d_model, d_year, amount):
         super().__init__(d_model, d_year, amount)
         self.bat_mah = bat_mah
         self.screen_inches = screen_inches
 
-    def __str__(self):
-        return f"на складе {self.amount} нетбуков"
-
-    def sended(self, send, office):
-        if str(send).isdigit() and self.amount >= send:
-            self.amount -= send
-            self.n_office[office] += send
-            return f'на складе {self.amount} нетбуков в офисе {office} {self.n_office[office]} нетбуков'
-        else:
-            return f'we cant'
-
 
 class Scanners(OfficeEquip):
+    name = "сканеров"
+
     def __init__(self, doc_format, d_model, d_year, amount):
         super().__init__(d_model, d_year, amount)
         self.doc_format = doc_format
-
-    def __str__(self):
-        return f"на складе {self.amount} сканеров"
-
-    def sended(self, send, office):
-        if str(send).isdigit() and self.amount >= send:
-            self.amount -= send
-            self.s_office[office] += send
-            return f'на складе {self.amount} сканеров в офисе {office} {self.s_office[office]} сканеров'
-        else:
-            return f'we cant'
 
 
 printer_1 = Printers('laser', 'yes', 1100, 2022, 10)
@@ -71,4 +57,3 @@ print(printer_1.sended(10, 'office_1'))
 netbook_1 = Netbooks(5000, 15, '1500super', 2022, 20)
 print(netbook_1)
 print(netbook_1.sended(2, 'office_2'))
-
